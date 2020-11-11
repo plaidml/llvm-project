5b9abe09ef0a929f6ad9e0a95298257c06145246
Vladislav Vinogradov <vlad.vinogradov@intel.com>
2020-11-11 16:58:41 +0300

    [llvm] Format provider improvements
    * Remove `std::forward` call for `iterator_range` iterator de-reference.
    * Use exact type `T` in `has_StreamOperator` instead of constant reference.

--- llvm/include/llvm/Support/FormatProviders.h
+++ llvm/include/llvm/Support/FormatProviders.h
@@ -355,7 +355,6 @@ struct range_item_has_provider
 
 template <typename IterT> class format_provider<llvm::iterator_range<IterT>> {
   using value = typename std::iterator_traits<IterT>::value_type;
-  using reference = typename std::iterator_traits<IterT>::reference;
 
   static StringRef consumeOneOption(StringRef &Style, char Indicator,
                                     StringRef Default) {
@@ -404,14 +403,14 @@ public:
     auto End = V.end();
     if (Begin != End) {
       auto Adapter =
-          detail::build_format_adapter(std::forward<reference>(*Begin));
+          detail::build_format_adapter(*Begin);
       Adapter.format(Stream, ArgStyle);
       ++Begin;
     }
     while (Begin != End) {
       Stream << Sep;
       auto Adapter =
-          detail::build_format_adapter(std::forward<reference>(*Begin));
+          detail::build_format_adapter(*Begin);
       Adapter.format(Stream, ArgStyle);
       ++Begin;
     }
