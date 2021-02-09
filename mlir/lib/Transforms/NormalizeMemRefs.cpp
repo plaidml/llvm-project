9920ebefbbadea8ed8474f54abe3b6b465531396
Adam Straw <adam.d.straw@intel.com>
2021-02-09 14:50:49 -0800

    make affine parallel and yield MemRef normalizable

--- mlir/lib/Transforms/NormalizeMemRefs.cpp
+++ mlir/lib/Transforms/NormalizeMemRefs.cpp
@@ -483,6 +483,11 @@ Operation *NormalizeMemRefs::createOpResultsNormalized(FuncOp funcOp,
   OperationState result(oldOp->getLoc(), oldOp->getName());
   result.addOperands(oldOp->getOperands());
   result.addAttributes(oldOp->getAttrs());
+  for (auto &oldRegion : oldOp->getRegions()) {
+    auto newRegion = result.addRegion();
+    newRegion->getBlocks().splice(newRegion->begin(), oldRegion.getBlocks());
+  }
+
   // Add normalized MemRefType to the OperationState.
   SmallVector<Type, 4> resultTypes;
   OpBuilder b(funcOp);
