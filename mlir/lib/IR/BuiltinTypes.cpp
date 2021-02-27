ad94d66e9ad408da776a8b7d3f8a96594e5b4136
Vladislav Vinogradov <vlad.vinogradov@intel.com>
2021-02-27 14:35:24 +0300

    [mlir] Allow custom dialect types as MemRef elements

--- mlir/lib/IR/BuiltinTypes.cpp
+++ mlir/lib/IR/BuiltinTypes.cpp
@@ -472,6 +472,14 @@ UnrankedTensorType::verify(function_ref<InFlightDiagnostic()> emitError,
 // BaseMemRefType
 //===----------------------------------------------------------------------===//
 
+bool BaseMemRefType::isValidElementType(Type type) {
+  // Note: Non standard/builtin types are allowed to exist within memref
+  // types. Dialects are expected to verify that memref types have a valid
+  // element type within that dialect.
+  return type.isIntOrIndexOrFloat() || type.isa<ComplexType, VectorType>() ||
+         !type.getDialect().getNamespace().empty();
+}
+
 unsigned BaseMemRefType::getMemorySpaceAsInt() const {
   return static_cast<ImplType *>(impl)->memorySpace;
 }
